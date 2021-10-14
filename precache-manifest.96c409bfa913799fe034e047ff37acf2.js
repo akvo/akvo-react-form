7c12479aea862c8e1fafdb01b3e381237fa84dcb
dedenbangkit <mail@dedenbangkit.com>
2021-10-14 15:35:28 +0700

    Updates

---
+++ precache-manifest.96c409bfa913799fe034e047ff37acf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "314904dd6ae2bac02be04885bd0661b5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e8c3c519f1e521d51d6f",
+    "url": "./static/css/2.09453326.chunk.css"
+  },
+  {
+    "revision": "1115c77cc777c18c3b2d",
+    "url": "./static/css/main.c5ca9d5a.chunk.css"
+  },
+  {
+    "revision": "e8c3c519f1e521d51d6f",
+    "url": "./static/js/2.e17f7c99.chunk.js"
+  },
+  {
+    "revision": "41e8aedee29e6e7a9a1bdd0a210ba283",
+    "url": "./static/js/2.e17f7c99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1115c77cc777c18c3b2d",
+    "url": "./static/js/main.7c6a2ab2.chunk.js"
+  },
+  {
+    "revision": "40371111ff28578a5e73",
+    "url": "./static/js/runtime-main.0b020250.js"
+  }
+]);
