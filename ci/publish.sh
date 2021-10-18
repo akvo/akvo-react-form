6cf60c3e397bd937299607da1d2f903f3b70c133
dedenbangkit <mail@dedenbangkit.com>
2021-10-18 17:09:46 +0700

    [#1] Publish pragmatically

--- ci/publish.sh
+++ ci/publish.sh
@@ -3,4 +3,4 @@ docker run \
        --volume "$(pwd):/home/akvo-react-form" \
        --workdir "/home/akvo-react-form" \
        --entrypoint /bin/sh \
-       node:lts-alpine3.13 -c 'npm publish'
+       node:lts-alpine3.13 -c "npm config set '//registry.npmjs.org/:_authToken' ${NPM_PUBLISH_TOKEN} && npm publish"
