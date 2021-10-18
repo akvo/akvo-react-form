4a475b8a21acfba78a732e6ba8b0268ce6170774
dedenbangkit <mail@dedenbangkit.com>
2021-10-18 17:53:24 +0700

    [#1] Check tag before publish

--- ci/publish.sh
+++ ci/publish.sh
@@ -1,6 +1,23 @@
-docker run \
-       --rm \
-       --volume "$(pwd):/home/akvo-react-form" \
-       --workdir "/home/akvo-react-form" \
-       --entrypoint /bin/sh \
-       node:lts-alpine3.13 -c "npm config set '//registry.npmjs.org/:_authToken' ${NPM_PUBLISH_TOKEN} && npm publish"
+publish() {
+    docker run \
+           --rm \
+           --volume "$(pwd):/home/akvo-react-form" \
+           --workdir "/home/akvo-react-form" \
+           --entrypoint /bin/sh \
+           node:lts-alpine3.13 -c "npm config set '//registry.npmjs.org/:_authToken' ${NPM_PUBLISH_TOKEN} && npm publish"
+}
+
+check_version() {
+    LAST_VERSION=$(npm info 'akvo-react-form' version)
+    NEW_VERSION=$(echo "$CI_TAG" | sed "s/v//g")
+    if [[ "$LAST_VERSION" != "$NEW_VERSION" ]]; then
+        echo "PUBLISHING $CI_TAG"
+        publish
+    else
+        echo "SKIP PUBLISHING $CI_COMMIT"
+    fi
+}
+
+if [[ "${CI_TAG:=}" =~ v.* ]]; then
+    check_version
+fi
