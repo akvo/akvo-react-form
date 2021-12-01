428f7d0bc7b29333e17550f87472281085c7d691
dedenbangkit <mail@dedenbangkit.com>
2021-12-01 07:53:12 +0700

    Updates

---
+++ precache-manifest.66a255d7784161a71b3bb834844832d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ab1f21d2652559f32da4488e8cefb6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c5721d40ec1c866f27b2",
+    "url": "./static/css/2.c276d5a3.chunk.css"
+  },
+  {
+    "revision": "cd59dcfde83eca8c413a",
+    "url": "./static/css/main.8e896ab7.chunk.css"
+  },
+  {
+    "revision": "c5721d40ec1c866f27b2",
+    "url": "./static/js/2.9fa9d5f7.chunk.js"
+  },
+  {
+    "revision": "eeacf40f8b82fcbf00f544a16daccfc3",
+    "url": "./static/js/2.9fa9d5f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd59dcfde83eca8c413a",
+    "url": "./static/js/main.2da39ede.chunk.js"
+  },
+  {
+    "revision": "40371111ff28578a5e73",
+    "url": "./static/js/runtime-main.0b020250.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9e4826fb845af0dedceca9f3f2948fb2.js"
+  "./precache-manifest.66a255d7784161a71b3bb834844832d6.js"
 );
 
 self.addEventListener('message', (event) => {
