5376c1d7e3bcf3a7cd6185b994ae3c92b92e234e
wayangalihpratama <wgprtm@gmail.com>
2022-07-29 21:24:05 +0800

    [#47] Fix allow search on cascade select dropdown

--- src/fields/TypeCascade.jsx
+++ src/fields/TypeCascade.jsx
@@ -143,7 +143,7 @@ const TypeCascadeApi = ({
                   allowClear
                   showSearch
                   filterOption={true}
-                  optionFilterProp='children'
+                  optionFilterProp='label'
                 />
               </Row>
             )
