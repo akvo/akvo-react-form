0823fdd1a948daf0fc8bb7744bc40a6f6151af1d
dedenbangkit <mail@dedenbangkit.com>
2022-10-11 09:10:28 +0700

    [#60] Add more button, import table to question fields

--- src/fields/TypeTable.jsx
+++ src/fields/TypeTable.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Col, Form, Input, Table } from 'antd';
+import { Row, Col, Form, Input, Table, Button } from 'antd';
 import { Extra, FieldLabel } from '../support';
 
 const TypeTable = ({
@@ -12,14 +12,25 @@ const TypeTable = ({
   extra,
   columns,
 }) => {
-  const dataSource = useState([]);
   columns = columns.map((x) => {
     return {
       title: x?.label || x.name,
       dataIndex: x.name,
       key: x.name,
+      render: () => <Input />,
     };
   });
+  columns = [
+    ...columns,
+    {
+      title: 'Action',
+      dataIndex: 'action',
+      key: 'action',
+      render: () => <Button type="link">Delete</Button>,
+    },
+  ];
+
+  const [dataSource, setDataSource] = useState([]);
 
   const extraBefore = extra
     ? extra.filter((ex) => ex.placement === 'before')
@@ -27,6 +38,16 @@ const TypeTable = ({
   const extraAfter = extra
     ? extra.filter((ex) => ex.placement === 'after')
     : [];
+
+  const onMore = () => {
+    const defaultSource = columns.reduce(
+      (curr, x) => {
+        return { ...curr, [x.key]: null };
+      },
+      { key: dataSource.length + 1 }
+    );
+    setDataSource([...dataSource, defaultSource]);
+  };
 
   return (
     <Col>
@@ -56,12 +77,36 @@ const TypeTable = ({
         >
           <Input disabled />
         </Form.Item>
-        <Table
-          size="small"
-          pagination={false}
-          dataSource={dataSource}
-          columns={columns}
-        />
+        <Row
+          justify="space-between"
+          style={{ marginBottom: '10px' }}
+          gutter={[20, 12]}
+        >
+          <Col
+            xs={24}
+            sm={24}
+            md={24}
+            lg={24}
+            xl={24}
+          >
+            <Table
+              size="small"
+              pagination={false}
+              dataSource={dataSource}
+              columns={columns}
+              bordered
+            />
+          </Col>
+          <Col
+            xs={24}
+            sm={24}
+            md={24}
+            lg={24}
+            xl={24}
+          >
+            <Button onClick={onMore}>Add More</Button>
+          </Col>
+        </Row>
         {!!extraAfter?.length &&
           extraAfter.map((ex, exi) => (
             <Extra
