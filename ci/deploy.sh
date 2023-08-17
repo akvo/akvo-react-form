82244daeacfe28ba24e3cbf1336279f1ede08763
Anjar Fiandrianto <anjar@akvo.org>
2023-08-17 16:45:22 +0700

    [#126] Test deploy poc-* branch to server

---
+++ ci/deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#shellcheck disable=SC2039
+
+set -exuo pipefail
+
+if [[ "${CI_BRANCH}" != poc-* ]]; then
+    exit 0
+fi
+
+
+docker run \
+       --rm \
+       --volume "$(pwd):/home/akvo-react-form" \
+       --workdir "/home/akvo-react-form" \
+       --entrypoint /bin/sh \
+       node:14-alpine3.13 -c 'yarn install && cd example && yarn build'
+
+ssh -i "${SITES_SSH_KEY}" -o BatchMode=yes \
+    -p 18765 \
+    -o UserKnownHostsFile=/dev/null \
+    -o StrictHostKeyChecking=no \
+    u7-nnfq7m4dqfyx@35.214.170.100 "mkdir -p www/akvo-react-form.tc.akvo.org/public_html/${CI_BRANCH}/"
+
+rsync \
+    --archive \
+    --compress \
+    --progress \
+    --exclude=ci \
+    --exclude=node_modules \
+    --rsh="ssh -i ${SITES_SSH_KEY} -o BatchMode=yes -p 18765 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
+    example/build/* u7-nnfq7m4dqfyx@35.214.170.100:/home/customer/www/akvo-react-form.tc.akvo.org/public_html/${CI_BRANCH}/
