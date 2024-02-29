1f3df55649db2ddce6d6ed6b06fd4e9b06c9f4f5
dedenbangkit <mail@dedenbangkit.com>
2024-02-29 07:10:02 +0700

    Fix lint

--- src/support/DraggerText.jsx
+++ src/support/DraggerText.jsx
@@ -3,13 +3,13 @@ import { InboxOutlined } from '@ant-design/icons';
 
 const DraggerText = ({ uiText, limit = 2 }) => {
   return (
-    <React.Fragment>
+    <>
       <p className="ant-upload-drag-icon">
         <InboxOutlined />
       </p>
       <p className="ant-upload-text">{uiText.dragFileToUpload}</p>
       <p className="ant-upload-hint">{`${uiText.fileUploadOnlySupport} ${limit} MB.`}</p>
-    </React.Fragment>
+    </>
   );
 };
 
