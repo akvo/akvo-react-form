b8ce82baee7afd1ff29434cf8f250310df1c7c85
dedenbangkit <mail@dedenbangkit.com>
2024-02-29 07:21:58 +0700

    Revert deprecated dragger

--- src/support/DraggerText.jsx
+++ src/support/DraggerText.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { InboxOutlined } from '@ant-design/icons';
 
 const DraggerText = ({ uiText, limit = 2 }) => {
   return (
-    <>
+    <Fragment>
       <p className="ant-upload-drag-icon">
         <InboxOutlined />
       </p>
       <p className="ant-upload-text">{uiText.dragFileToUpload}</p>
       <p className="ant-upload-hint">{`${uiText.fileUploadOnlySupport} ${limit} MB.`}</p>
-    </>
+    </Fragment>
   );
 };
 
