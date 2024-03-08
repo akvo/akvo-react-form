f9ffa21329f7e0be100efbdbb9e565a1aa3f8136
navins94 <nikshingote@gmail.com>
2024-03-08 09:28:24 +0530

    [#136] Update all questions to use reduce

--- src/index.js
+++ src/index.js
@@ -154,9 +154,14 @@ export const Webform = ({
       .filter((qg) => !qg?.repeatable)
       .flatMap((qg) => qg.question.filter((q) => q?.meta))
       .map((q) => ({ id: q.id, type: q.type, value: null }));
-    const allQuestions = [
-      ...new Set(forms.question_group.flatMap((qg) => qg.question)),
-    ];
+    const allQuestions = forms.question_group.reduce((uniqueQuestions, qg) => {
+      qg.question.forEach((question) => {
+        if (!uniqueQuestions.includes(question)) {
+          uniqueQuestions.push(question);
+        }
+      });
+      return uniqueQuestions;
+    }, []);
     GlobalStore.update((gs) => {
       gs.dataPointName = meta;
       gs.allQuestions = allQuestions;
